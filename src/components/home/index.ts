30319fd31564db0ace73ed37db68aa5bee4785d5
hafizaatifkamal <hafizaatifkamal@gmail.com>
2026-05-21 00:55:06 +0530

    feat: contents added to new section

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,5 +1,6 @@
 import Hero from "./Hero";
 import ProductRange from "./ProductRange";
+import ProductCarousel from "./ProductCarousel";
 import RightChoice from "./RightChoice";
 
-export { Hero, ProductRange, RightChoice };
+export { Hero, ProductRange, ProductCarousel, RightChoice };
